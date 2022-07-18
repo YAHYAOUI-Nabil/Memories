3bf9b847b7052c9fb08cb2f338a0d4e57895c3a5
Yahyaoui Nabil <yahyaouinabil1993@gmail.com>
2022-07-19 00:19:34 +0200

    Google auth

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,112 @@
+import { Avatar, Button, Container, Grid, Paper, Typography } from '@material-ui/core'
+import React,{useState, useEffect} from 'react'
+import { useHistory } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import {GoogleLogin} from 'react-google-login'
+import {gapi} from 'gapi-script'
+
+import useStyles from './styles'
+import Input from './Input'
+import Icon from './Icon'
+
+
+const Auth = () => {
+    const classes = useStyles()
+    const [isSignup, setisSignup] = useState(false)
+    const [showPassword, setshowPassword] = useState(false)
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    const handleShowPassword = () => {
+        setshowPassword((prevShowPwd)=> !prevShowPwd)
+    }
+
+    const switchMode = () => {
+        setisSignup((isSignup)=>!isSignup)
+    }
+
+    const handleSubmit = () => {}
+    const handleChange = () => {}
+
+    const googleSuccess = async (res) => {
+        const result = res?.profileObj
+        const token = res?.tokenId
+
+        try {
+            dispatch({type : 'AUTH', data : {result, token}})
+            history.push('/')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const googleFailure = (err) => {
+        console.log(err)
+        console.log('Google sign in failed! try again later.')
+    }
+
+    // useEffect(() => {
+    //   const start=()=>{
+    //     gapi.client.init({
+    //         clientId:'152431167825-hsj7n6qmb38mkia4q4cvt2lgp5afqamf.apps.googleusercontent.com',
+    //         scope:''
+    //     })
+    //   }
+    //   gapi.load('client:auth2', start)
+    
+    // }, [])
+    
+
+  return (
+    <Container component='main' maxWidth='xs'>
+        <Paper className={classes.paper}>
+            <Avatar className={classes.avatar} elevation={3}>
+                <LockOutlinedIcon />
+            </Avatar>
+            <Typography variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+            <form className={classes.form} onSubmit={handleSubmit}>
+                <Grid container spacing={2}>
+                    {isSignup && (
+                        <>
+                            <Input name='firstName' label='First Name' handleChange={handleChange} autoFocus half/>
+                            <Input name='lastName' label='Last Name' handleChange={handleChange}  half/>
+                        </>
+                    )}
+                    <Input name='email' label='Email Adress' handleChange={handleChange} type='email'/>
+                    <Input name='password' label='Password' handleChange={handleChange} 
+                           type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword}/>
+                    {isSignup && <Input name='confirmPassword' label='Confirm password' handleChange={handleChange}
+                                        type='password'/>}
+                </Grid>
+                
+                <Button type='submit' variant='contained' fullWidth color='primary' className={classes.submit}>
+                    {isSignup ? 'Sign Up' : 'Sign In'}
+                </Button>
+                <GoogleLogin 
+                    clientId='152431167825-hsj7n6qmb38mkia4q4cvt2lgp5afqamf.apps.googleusercontent.com' 
+                    render={(renderProps) => (
+                        <Button className={classes.googleButton} color='primary' fullWidth 
+                        onClick={renderProps.onClick} disabled={renderProps.disabled} 
+                        startIcon={<Icon/>} variant='contained'>
+                            Google Sign In
+                        </Button>
+                    )}
+                    onSuccess={googleSuccess}
+                    onFailure={googleFailure}
+                    cookiePolicy={'single_host_origin'}
+                />
+                <Grid container justifyContent='flex-end'>
+                    <Grid item>
+                        <Button onClick={switchMode}>
+                            {isSignup ? 'Already have an account? Sign In' : `Don't have an account? Sign Up`}
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </Paper>
+    </Container>
+  )
+}
+
+export default Auth
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect} from 'react'
+import { Container, Grid, Grow } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
+
+
+import Posts from '../Posts/Posts'
+import Form from '../Form/Form'
+import {getPosts} from '../../actions/posts' 
+
+const Home = () => {
+    const dispatch = useDispatch()
+    const [currentId, setCurrentId] = useState(0);
+
+    useEffect(() => {
+        dispatch(getPosts())
+    }, [currentId, dispatch])
+  return (
+    <Grow in>
+        <Container>
+          <Grid container justifyContent='space-between' alignItems='stretch' spacing={4}>
+            <Grid item xs={12} sm={7}>
+             <Posts setCurrentId={setCurrentId} /> 
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+  )
+}
+
+export default Home
